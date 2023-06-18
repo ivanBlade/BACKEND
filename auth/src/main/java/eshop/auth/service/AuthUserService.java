05101d0a559e3c08efb9e6d39c4509016c1f3300
ivanBlade <itsesparta@gmail.com>
2023-06-18 12:30:15 -0500

    correct2

--- auth/src/main/java/eshop/auth/service/AuthUserService.java
+++ auth/src/main/java/eshop/auth/service/AuthUserService.java
@@ -5,8 +5,10 @@ import eshop.auth.entity.AuthUser;
 import eshop.auth.entity.TokenDto;
 
 public interface AuthUserService {
-    public AuthUser save(AuthUserDto dto);
-    public TokenDto login(AuthUserDto dto);
+    public AuthUser save(AuthUserDto authUserDto);
+
+    public TokenDto login(AuthUserDto authUserDto);
+
     public TokenDto validate(String token);
 
 }
